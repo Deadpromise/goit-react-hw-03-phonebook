6722a1c5acc16dee649c49edb244c63b19b0bb1a
Dmitry Yavtushenko <rosengan22@gmail.com>
2023-03-19 14:09:36 +0200

    task 1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import Filter from "./Filter/Filter";
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 class App extends Component {
   static propTypes = {
@@ -11,41 +18,55 @@ class App extends Component {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-    })
-
+    }),
   };
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-    filter: ''
+    contacts: initialContacts,
+    filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const currentContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    // console.log('new', currentContacts);
+    // console.log('prev', prevContacts);
+    if (currentContacts !== prevContacts) {
+      console.log('dobavili');
+      localStorage.setItem('contacts', JSON.stringify(currentContacts));
+    }
+  }
 
   addContact = (name, number) => {
-
     const contact = {
       id: nanoid(),
       name,
       number,
-    }
-    this.setState((prevState) => {
-      const chekClone = prevState.contacts.find(item => item.name === contact.name || item.number === contact.number);
+    };
+    this.setState(prevState => {
+      const chekClone = prevState.contacts.find(
+        item => item.name === contact.name || item.number === contact.number
+      );
       if (chekClone) {
-        alert(`User with name ${contact.name} or number ${contact.number} is already in contacts`);
-        return {contacts: [...prevState.contacts]};
+        alert(
+          `User with name ${contact.name} or number ${contact.number} is already in contacts`
+        );
+        return { contacts: [...prevState.contacts] };
       }
-      return {contacts: [contact, ...prevState.contacts]}
-    })
-   
+      return { contacts: [contact, ...prevState.contacts] };
+    });
   };
 
-  deleteContact = (contactId) => {
+  deleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }))
+    }));
   };
   getFilter = e => {
     this.setState({ filter: e.currentTarget.value });
@@ -55,32 +76,37 @@ class App extends Component {
     const normalizedFilter = filter.toLocaleLowerCase();
 
     return contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(normalizedFilter));
-  }
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
 
   render() {
-    const {filter } = this.state;
+    const { filter } = this.state;
     const filteredContacts = this.getFiltredContacts();
     // console.log(this.state);
-    return <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    ><h1>Phonebook</h1>
-      <ContactForm onSubmit={this.addContact}></ContactForm>
-      <h2>Contacts</h2>
-      <Filter value={filter} onChange={this.getFilter}></Filter>
-      <ContactList contacts={filteredContacts} onDelete={this.deleteContact}>
-
-      </ContactList>
-    </div>
-  };
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact}></ContactForm>
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.getFilter}></Filter>
+        <ContactList
+          contacts={filteredContacts}
+          onDelete={this.deleteContact}
+        ></ContactList>
+      </div>
+    );
+  }
 }
 
 export default App;
